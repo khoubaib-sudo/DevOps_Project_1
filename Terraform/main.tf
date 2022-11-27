71b14d0fd4909f98feeb5257ec2a118021564ec1
khoubaib maamouri <83957252+khoubaib-sudo@users.noreply.github.com>
2022-11-27 12:55:59 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "amazon-linux" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20221103.3-x86_64-gp2"]
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
     
   }
 
